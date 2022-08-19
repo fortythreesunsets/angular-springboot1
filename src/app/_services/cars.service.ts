67ab3d1c8f2ada8f2544df5e72ab571c5afc84b3
fortythreesunsets <sruv_2601@hotmail.com>
2022-08-18 21:38:20 -0500

    Modified heroku url

--- src/app/_services/cars.service.ts
+++ src/app/_services/cars.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+// Ejecución local
 // const API_URL = 'http://localhost:8080/api/';
+
+//Ejecución en vercel.com
 const API_URL = 'https://spring-security--jwt.herokuapp.com/api/';
 
 @Injectable({
